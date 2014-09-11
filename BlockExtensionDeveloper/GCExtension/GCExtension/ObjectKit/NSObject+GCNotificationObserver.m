bf23595cbd29703a28387a96f6bbe00512c3eb82
zhoujinqiang <garychow.go@qq.com>
2014-09-11 17:54:41 +0800

    Add GCNotificationBlock's class mehtod

--- BlockExtensionDeveloper/GCExtension/GCExtension/ObjectKit/NSObject+GCNotificationObserver.m
+++ BlockExtensionDeveloper/GCExtension/GCExtension/ObjectKit/NSObject+GCNotificationObserver.m
@@ -47,6 +47,44 @@
 
 @implementation NSObject (GCNotificationObserver)
 
+
+#pragma mark - class public method
++ (void)addObserverForNotificationName:(NSString *)name usingBlock:(GCNotificationObserverBlock)block {
+    NSParameterAssert(name);
+    NSParameterAssert(block);
+    
+    GCNotificationObserverWrapper* wrapper = [[GCNotificationObserverWrapper alloc] initWithObserverForNotificationName:name handlerBlock:block];
+    
+    [[self _observerWappersForNotificationName:name] addObject:wrapper];
+}
++ (void)removeObserverForNotificationName:(NSString *)name {
+    NSParameterAssert(name);
+    
+    [[self _observerWappersForNotificationName:name] removeAllObjects];
+}
+
+
+#pragma mark - class private method
++ (NSMutableArray *)_observerWappersForNotificationName:(NSString *)name {
+    static char const observersMapKey;
+    NSMutableDictionary* observersMap = objc_getAssociatedObject(self, &observersMapKey);
+    if (!observersMap) {
+        observersMap = [NSMutableDictionary dictionary];
+        objc_setAssociatedObject(self, &observersMapKey, observersMap, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    
+    NSMutableArray* observersArr = observersMap[name];
+    if (!observersArr) {
+        observersArr = [NSMutableArray array];
+        observersMap[name] = observersArr;
+    }
+    
+    return observersArr;
+}
+
+
+
+
 #pragma mark - instance public method
 - (void)addObserverForNotificationName:(NSString *)name usingBlock:(GCNotificationObserverBlock)block {
     NSParameterAssert(name);
