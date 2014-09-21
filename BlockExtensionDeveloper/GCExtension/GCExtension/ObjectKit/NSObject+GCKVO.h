fcf38440f960fedb26cfd6c93800f407eacb714d
njgarychow <garychow.go@qq.com>
2014-09-21 23:01:02 +0800

    Add some comemnts.

--- BlockExtensionDeveloper/GCExtension/GCExtension/ObjectKit/NSObject+GCKVO.h
+++ BlockExtensionDeveloper/GCExtension/GCExtension/ObjectKit/NSObject+GCKVO.h
@@ -10,16 +10,44 @@
 
 typedef void (^GCKVOBlock)(NSDictionary* change);
 
+/**
+ *  This class extension is for KVO.
+ *  You can add an observe using block. You don't need to override the method
+ *  |observeValueForKeyPath:ofObject:change:context:|. 
+ *  You don't have to remove the stop the observe. When either the observer or the 
+ *  target deallocation. The observe action will be stop automatic.
+ */
 @interface NSObject (GCKVO)
 
+/**
+ *  start observe an object's keyPath. using the options NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld.
+ *
+ *  @param observeTarger the target object want to be observed.
+ *  @param keyPath       the keyPath of the object. same as kvo.
+ *  @param handler       if the keyPath's value change, the handler block will be invoke.
+ */
 - (void)startObserveObject:(NSObject *)observeTarger
                 forKeyPath:(NSString *)keyPath
                 usingBlock:(GCKVOBlock)handler;
+/**
+ *  start observe an object's keyPath.
+ *
+ *  @param observeTarger the target object want to be observed.
+ *  @param keyPath       the keyPath of the object. same as kvo.
+ *  @param options       the same as kvo.
+ *  @param handler       if the keyPath's value change, the handler block will be invoke.
+ */
 - (void)startObserveObject:(NSObject *)observeTarger
                 forKeyPath:(NSString *)keyPath
                    options:(NSKeyValueObservingOptions)options
                 usingBlock:(GCKVOBlock)handler;
 
+/**
+ *  stop observe an object's keyPath.
+ *
+ *  @param observeTarger the target object was observed.
+ *  @param keyPath       the same as kvo.
+ */
 - (void)stopObserveObject:(NSObject *)observeTarger
                forKeyPath:(NSString *)keyPath;
 
