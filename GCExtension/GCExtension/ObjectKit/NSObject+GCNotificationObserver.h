fd7c00d0da259d6d2e84bd34cbf39cf3c3879e8a
njgarychow <garychow.go@qq.com>
2015-03-13 00:31:14 +0800

    NSObject+GCNotificationObserver add method to observer the `object' post the notification

--- GCExtension/GCExtension/ObjectKit/NSObject+GCNotificationObserver.h
+++ GCExtension/GCExtension/ObjectKit/NSObject+GCNotificationObserver.h
@@ -18,35 +18,69 @@ typedef void (^GCNotificationObserverBlock)(NSNotification* notification);
  */
 @interface NSObject (GCNotificationObserver)
 
+
 /**
  *  add class |self| to observe the notification named |name|. The |block| will be invoked when
  *  the notification is posted.
  *
- *  @param name     the name of the notification which want to be observed. |name| can't be nil.
+ *  @param name     The name of the notification which want to be observed. |name| can't be nil.
+ *  @param anObject It's the object which post the notification. if |anObject| is nil, observe all objects'
+                    posting notifications.
  *  @param block    |block| is the callback when the notification is posted. |block| can't be nil.
  */
-+ (void)addObserverForNotificationName:(NSString *)name usingBlock:(GCNotificationObserverBlock)block;
++ (void)addObserverForNotificationName:(NSString *)name
+                                object:(id)anObject
+                            usingBlock:(GCNotificationObserverBlock)block;
+/**
+ *  Equal to |addObserverForNotificationName:object:usingBlocks:| with nil to |anObject|.
+ */
++ (void)addObserverForNotificationName:(NSString *)name
+                            usingBlock:(GCNotificationObserverBlock)block;
 
 /**
  *  remove class |self| to observe the notifications those named |name|.
  *
  *  @param name     the name of the notification which want to stop observing. |name| can't be nil.
+ *  @param anObject It's the object which post the notification. if |anObject| is nil, remove all observers 
+                    named |name|
+ */
++ (void)removeObserverForNotificationName:(NSString *)name object:(id)anObject;
+/**
+ *  Equal to |removeObserverForNotificationName:object| with nil to |anObject|.
  */
 + (void)removeObserverForNotificationName:(NSString *)name;
+
+
+
 
 /**
  *  add |self| to observe the notification named |name|. The |block| will be invoked when
  *  the notification is posted.
  *
- *  @param name     the name of the notification which want to be observed. |name| can't be nil.
+ *  @param name     The name of the notification which want to be observed. |name| can't be nil.
+ *  @param anObject It's the object which post the notification. if |anObject| is nil, observe all objects'
+ posting notifications.
  *  @param block    |block| is the callback when the notification is posted. |block| can't be nil.
  */
-- (void)addObserverForNotificationName:(NSString *)name usingBlock:(GCNotificationObserverBlock)block;
+- (void)addObserverForNotificationName:(NSString *)name
+                                object:(id)anObject
+                            usingBlock:(GCNotificationObserverBlock)block;
+/**
+ *  Equal to |addObserverForNotificationName:object:usingBlocks:| with nil to |anObject|.
+ */
+- (void)addObserverForNotificationName:(NSString *)name
+                            usingBlock:(GCNotificationObserverBlock)block;
 
 /**
  *  remove |self| to observe the notifications those named |name|.
  *
  *  @param name     the name of the notification which want to stop observing. |name| can't be nil.
+ *  @param anObject It's the object which post the notification. if |anObject| is nil, remove all observers
+ named |name|
+ */
+- (void)removeObserverForNotificationName:(NSString *)name object:(id)anObject;
+/**
+ *  Equal to |removeObserverForNotificationName:object| with nil to |anObject|.
  */
 - (void)removeObserverForNotificationName:(NSString *)name;
 
--- GCExtension/GCExtension/ObjectKit/NSObject+GCNotificationObserver.m
+++ GCExtension/GCExtension/ObjectKit/NSObject+GCNotificationObserver.m
@@ -11,23 +11,31 @@
 #import <objc/runtime.h>
 
 
-@interface GCNotificationObserverWrapper : NSObject
+@interface GCNotificationObserverWrapper : NSObject {
+    __weak id _object;
+}
 
-@property (nonatomic, strong) GCNotificationObserverBlock handleBlock;
+@property (nonatomic, readonly) id object;
+@property (nonatomic, readonly) GCNotificationObserverBlock handleBlock;
 
-- (instancetype)initWithObserverForNotificationName:(NSString *)name handlerBlock:(GCNotificationObserverBlock)block;
+- (instancetype)initWithObserverForNotificationName:(NSString *)name
+                                             object:(id)object
+                                       handlerBlock:(GCNotificationObserverBlock)block;
 
 @end
 
 @implementation GCNotificationObserverWrapper
 
-- (instancetype)initWithObserverForNotificationName:(NSString *)name handlerBlock:(GCNotificationObserverBlock)block {
+- (instancetype)initWithObserverForNotificationName:(NSString *)name
+                                             object:(id)object
+                                       handlerBlock:(GCNotificationObserverBlock)block {
     if (self = [self init]) {
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(_observerHandlerAction:)
                                                      name:name
-                                                   object:nil];
-        _handleBlock = block;
+                                                   object:object];
+        _handleBlock = [block copy];
+        _object = object;
     }
     return self;
 }
@@ -49,28 +57,58 @@
 
 
 #pragma mark - class public method
-+ (void)addObserverForNotificationName:(NSString *)name usingBlock:(GCNotificationObserverBlock)block {
+
++ (void)addObserverForNotificationName:(NSString *)name
+                                object:(id)anObject
+                            usingBlock:(GCNotificationObserverBlock)block {
     NSParameterAssert(name);
     NSParameterAssert(block);
     
-    GCNotificationObserverWrapper* wrapper = [[GCNotificationObserverWrapper alloc] initWithObserverForNotificationName:name handlerBlock:block];
+    GCNotificationObserverWrapper* wrapper = [[GCNotificationObserverWrapper alloc] initWithObserverForNotificationName:name object:anObject handlerBlock:block];
     
-    [[self _observerWappersForNotificationName:name] addObject:wrapper];
+    [[self _observerWrappersForNotificationName:name] addObject:wrapper];
+}
++ (void)addObserverForNotificationName:(NSString *)name usingBlock:(GCNotificationObserverBlock)block {
+    [self addObserverForNotificationName:name object:nil usingBlock:block];
 }
 + (void)removeObserverForNotificationName:(NSString *)name {
+    [self removeObserverForNotificationName:name object:nil];
+}
++ (void)removeObserverForNotificationName:(NSString *)name object:(id)anObject {
     NSParameterAssert(name);
     
-    [[self _observerWappersForNotificationName:name] removeAllObjects];
+    NSMutableArray* observerWrappers = [self _observerWrappersForNotificationName:name];
+    [self _removeWrappersUsingObject:anObject fromAllWrappers:observerWrappers];
 }
 
 
 #pragma mark - class private method
-+ (NSMutableArray *)_observerWappersForNotificationName:(NSString *)name {
++ (NSMutableArray *)_observerWrappersForNotificationName:(NSString *)name {
+    return [self _observerWrappersForNotificationName:name object:self];
+}
+
+
+
++ (void)_removeWrappersUsingObject:(id)object fromAllWrappers:(NSMutableArray *)observerWrappers {
+    if (object) {
+        NSMutableArray* wrappers = [NSMutableArray new];
+        [observerWrappers enumerateObjectsUsingBlock:^(GCNotificationObserverWrapper* wrapper, NSUInteger idx, BOOL *stop) {
+            if (wrapper.object == object) {
+                [wrappers addObject:wrapper];
+            }
+        }];
+        [observerWrappers removeObjectsInArray:wrappers];
+    }
+    else {
+        [observerWrappers removeAllObjects];
+    }
+}
++ (NSMutableArray *)_observerWrappersForNotificationName:(NSString *)name object:(id)object {
     static char const observersMapKey;
-    NSMutableDictionary* observersMap = objc_getAssociatedObject(self, &observersMapKey);
+    NSMutableDictionary* observersMap = objc_getAssociatedObject(object, &observersMapKey);
     if (!observersMap) {
         observersMap = [NSMutableDictionary dictionary];
-        objc_setAssociatedObject(self, &observersMapKey, observersMap, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        objc_setAssociatedObject(object, &observersMapKey, observersMap, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
     
     NSMutableArray* observersArr = observersMap[name];
@@ -81,42 +119,37 @@
     
     return observersArr;
 }
-
 
 
 
 #pragma mark - instance public method
-- (void)addObserverForNotificationName:(NSString *)name usingBlock:(GCNotificationObserverBlock)block {
+- (void)addObserverForNotificationName:(NSString *)name
+                                object:(id)anObject
+                            usingBlock:(GCNotificationObserverBlock)block {
     NSParameterAssert(name);
     NSParameterAssert(block);
     
-    GCNotificationObserverWrapper* wrapper = [[GCNotificationObserverWrapper alloc] initWithObserverForNotificationName:name handlerBlock:block];
+    GCNotificationObserverWrapper* wrapper = [[GCNotificationObserverWrapper alloc] initWithObserverForNotificationName:name object:anObject handlerBlock:block];
     
-    [[self _observerWappersForNotificationName:name] addObject:wrapper];
+    [[self _observerWrappersForNotificationName:name] addObject:wrapper];
+}
+- (void)addObserverForNotificationName:(NSString *)name usingBlock:(GCNotificationObserverBlock)block {
+    [self addObserverForNotificationName:name object:nil usingBlock:block];
 }
 - (void)removeObserverForNotificationName:(NSString *)name {
+    [self removeObserverForNotificationName:name object:nil];
+}
+- (void)removeObserverForNotificationName:(NSString *)name object:(id)anObject {
     NSParameterAssert(name);
     
-    [[self _observerWappersForNotificationName:name] removeAllObjects];
+    NSMutableArray* observerWrappers = [self _observerWrappersForNotificationName:name];
+    [[self class] _removeWrappersUsingObject:anObject fromAllWrappers:observerWrappers];
 }
 
 
 #pragma mark - instance private method
-- (NSMutableArray *)_observerWappersForNotificationName:(NSString *)name {
-    static char const observersMapKey;
-    NSMutableDictionary* observersMap = objc_getAssociatedObject(self, &observersMapKey);
-    if (!observersMap) {
-        observersMap = [NSMutableDictionary dictionary];
-        objc_setAssociatedObject(self, &observersMapKey, observersMap, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    }
-    
-    NSMutableArray* observersArr = observersMap[name];
-    if (!observersArr) {
-        observersArr = [NSMutableArray array];
-        observersMap[name] = observersArr;
-    }
-    
-    return observersArr;
+- (NSMutableArray *)_observerWrappersForNotificationName:(NSString *)name {
+    return [[self class] _observerWrappersForNotificationName:name object:self];
 }
 
 @end
